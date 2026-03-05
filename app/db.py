cdafbfc9244ebb5fb4ea50f63f4ed6f42e86a460
rileyrushton05 <rileyrushton05@gmail.com>
2026-03-05 17:18:12 +1000

    Setup database connection and session management

---
+++ app/db.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, create_engine, Session
+
+DATABASE_URL = "sqlite:///data.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
+
